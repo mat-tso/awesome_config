c3757f90800dc5351cc26cb6ba9bacef761ba025
mat-tso <mat-tso@topmail.ie>
2016-02-25 13:52:14 +0100

    Always start xfreerdp fullscreen
    Signed-off-by: mat-tso <mat-tso@topmail.ie>

--- rc.lua
+++ rc.lua
@@ -463,6 +463,8 @@ awful.rules.rules = {
     -- Set Firefox to always map on tags number 2 of screen 1.
     { rule = { class = "Firefox" },
       properties = { tag = tags[1][2] } },
+    { rule = { class = "xfreerdp" },
+      callback = function (c) c.fullscreen = true end },
 }
 -- }}}
 
